6de6c7104967572f6f4fb2683f4e4981440c718d
yuexianguo <gyx685255>
2017-03-10 09:37:22 +0800

    setup commit

---
+++ app/src/main/java/com/example/administrator/application01/MainActivity01.java
@@ -0,0 +1,18 @@
+package com.example.administrator.application01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity01 extends AppCompatActivity {
+
+    private TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main01);
+        TextView viewById = (TextView) findViewById(R.id.tv);
+    }
+    public void test(){
+
+}
---
+++ app/src/main/java/com/example/administrator/application01/hh.java
@@ -0,0 +1,10 @@
+package com.example.administrator.application01;
+
+/**
+ * Created by Administrator on 2017/3/10.
+ */
+
+public class hh {
+
+    dao();
+}
