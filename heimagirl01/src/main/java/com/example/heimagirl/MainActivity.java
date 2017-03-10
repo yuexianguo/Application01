f960d3119ee2975b512ea61be8d7855ffcd2d0c7
yuexianguo <gyx685255>
2017-03-10 20:23:07 +0800

    黑马美女项目实战1 commit

--- heimagirl01/src/main/java/com/example/heimagirl/MainActivity.java
+++ heimagirl01/src/main/java/com/example/heimagirl/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.heimagirl;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Request;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.list_view)
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+               Request request= new Request.Builder().get().url(url).build();
+            }
+        }).start();
     }
 }
